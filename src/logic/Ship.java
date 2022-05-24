d5d075803b03e8d095f8c5ebabfe36f6d0f41f37
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2022-05-25 10:04:01 +1200

    Add extra variables to Ship
    Speed, life and firepower.

--- src/logic/Ship.java
+++ src/logic/Ship.java
@@ -11,6 +11,9 @@ public class Ship {
   private String name;
   private int price;
   private int cargo;
+  private int speed;
+  private int life;
+  private int firepower;
   
   /**
    * Create a ship.
@@ -19,10 +22,13 @@ public class Ship {
    * @param price The price of the ship
    * @param cargo The amount of cargo the ship can carry
    */
-  protected Ship(String name, int price, int cargo) {
+  protected Ship(String name, int price, int cargo, int speed, int life, int firepower) {
     this.name = name;
     this.price = price;
     this.cargo = cargo;
+    this.speed = speed;
+    this.life = life;
+    this.firepower = firepower;
   }
 
   public String getName() {
@@ -47,6 +53,30 @@ public class Ship {
 
   public void setCargo(int cargo) {
     this.cargo = cargo;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public int getLife() {
+    return life;
+  }
+
+  public void setLife(int life) {
+    this.life = life;
+  }
+
+  public int getFirepower() {
+    return firepower;
+  }
+
+  public void setFirepower(int firepower) {
+    this.firepower = firepower;
   }
   
 }
