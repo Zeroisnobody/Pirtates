50b3ddb94c9ed36981870dec65718ca590ca48f9
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2022-05-25 16:35:03 +1200

    Create Archipelago
    Contains the game board.
    getIslandName returns the name of the current island (or sea).
    setSail makes the captain set sail.
    getSpeed returns the speed of the captain's current ship.
    getIslandNames returns a list of the island names.
    travelTo makes the captain dock at the given island.
    island_ returns the price of _ on the current island.
    wallet returns the amount of doubloons the Captain has.
    cargoSpace returns the space in use on the Captain's ship.
    cargoTotal returns the total space on the Captain's ship.
    getMaxItem returns the max amount of an item the Captain can purchase.
    buyItem buys a number of items.
    sellItem sells a number of items.
    costItem returns the cost of selling a number of items.
    captainItem returns the number of item onboard.
    crewMemberAvailable returns true if the current island crew isn't part
    of the crew.
    getCrewName returns the name of the island's crew member.
    getCrewCost returns the cost of the island's crew member.
    hireCrew hires the crew member of the current island.
    addCargoMember sets a boolean as true in all ships.
    addNavigator sets a boolean as true in all ships.
    addShipwright sets a boolean as true in all ships.
    shipDescirptions returns all ships description.
    shipDescription returns the current ship's description.
    buyShip buys the chosen ship.

---
+++ src/logic/Archipelago.java
@@ -0,0 +1,195 @@
+package logic;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The board game.
+
+ * @author Bryony
+ *
+ */
+public class Archipelago {
+
+  private Crew[] possibleMembers = Populate.crew();
+  private Island[] islands = Populate.islands();
+  private Ship[] ships = Populate.ships();
+
+  private Captain player;
+  private int currentLand;
+  private boolean atSea;
+
+  /**
+   * Create an Archipelago.
+   */
+  public Archipelago() {
+    player = new Captain(possibleMembers[0], possibleMembers[5], ships[0]);
+    possibleMembers[0] = null;
+    possibleMembers[1] = null;
+    currentLand = -1;
+    atSea = false;
+  }
+
+  /**
+   * Get the name of the current land (or sea).
+
+   * @return The name of the island
+   */
+  public String getIslandName() {
+    if (currentLand == -1) {
+      if (!atSea) {
+        return Populate.homeLand();
+      }
+      return "The Sea";
+    }
+    return islands[currentLand].getName();
+  }
+
+  /**
+   * Set sail.
+   */
+  public void setSail() {
+    currentLand = -1;
+    atSea = true;
+  }
+
+  public boolean atSea() {
+    return atSea();
+  }
+
+  public int getSpeed() {
+    return player.speed();
+  }
+
+  public List<String> getIslandNames() {
+    return Arrays.asList(islands).stream().map(i -> i.getName()).toList();
+  }
+
+  public void travelTo(int index) {
+    currentLand = index;
+    atSea = false;
+  }
+
+  public int islandGrain() {
+    return islands[currentLand].getGrain();
+  }
+
+  public int islandSpice() {
+    return islands[currentLand].getSpice();
+  }
+
+  public int islandMedicine() {
+    return islands[currentLand].getMedicine();
+  }
+
+  public int islandSilk() {
+    return islands[currentLand].getSilk();
+  }
+
+  public int wallet() {
+    return player.doubloons();
+  }
+
+  public int cargoSpace() {
+    return player.cargo();
+  }
+
+  public int cargoTotal() {
+    return player.cargoTotal();
+  }
+
+  public int getMaxItem(int index) {
+    return player.getBuyMax(islands[currentLand].get(index));
+  }
+
+  /**
+   * Buy a number of items.
+
+   * @param number The number of items to purchase
+   * @param index The index of the item
+   */
+  public void buyItem(int number, int index) {
+    player.buy(index, islands[currentLand].get(index), number);
+  }
+  
+  /**
+   * Sell a number of items.
+
+   * @param number The number of items to sell
+   * @param index The index of the item
+   */
+  public void sellItem(int number, int index) {
+    player.sell(index, islands[currentLand].get(index), number);
+  }
+  
+  public int costItem(int number, int index) {
+    return islands[currentLand].get(index) * number;
+  }
+  
+  public int captainItem(int index) {
+    return player.getSellMax(index);
+  }
+  
+  public boolean crewMemberAvailable() {
+    return possibleMembers[currentLand] == null;
+  }
+  
+  public String getCrewName() {
+    return possibleMembers[currentLand].getName();
+  }
+  
+  public int getCrewCost() {
+    return possibleMembers[currentLand].getSalary();
+  }
+
+  /**
+   * Hire the crew member on the current island.
+   */
+  public void hireCrew() {
+    switch (possibleMembers[currentLand].getName()) {
+      case "Cargo Master": 
+        addCargoMaster();
+        break;
+      case "Navigator":
+        addNavigator();
+        break;
+      case "Shipwright":
+        addShipwright();
+        break;
+      default:
+        break;
+    }
+    player.addMember(possibleMembers[currentLand]);
+  }
+  
+  private void addCargoMaster() {
+    for (Ship s : ships) {
+      s.addCargoMaster();
+    }
+  }
+  
+  private void addNavigator() {
+    for (Ship s : ships) {
+      s.addNavigator();
+    }
+  }
+  
+  private void addShipwright() {
+    for (Ship s : ships) {
+      s.addShipwright();
+    }
+  }
+  
+  public List<String> shipDescriptions() {
+    return Arrays.asList(ships).stream().map(s -> s.toString()).toList();
+  }
+  
+  public String shipDescription() {
+    return player.shipDescription();
+  }
+  
+  public boolean buyShip(int index) {
+    return player.buyShip(ships[index]);
+  }
+  
+}
