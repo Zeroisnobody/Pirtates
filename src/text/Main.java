fa05edf8f5f6d8e74e615c37efcdf8a139f59d64
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2022-05-25 16:46:48 +1200

    Create a Main class for TUI
    Contains methods for input and output. Means I can replace these for
    testing!

---
+++ src/text/Main.java
@@ -0,0 +1,60 @@
+package text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * The Main class for the Text-based User Interface.
+
+ * @author Bryony
+ *
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    //    new Speak(new Input(), new Output());
+  }
+
+  /**
+   * The output from the game.
+
+   * @author Bryony
+   *
+   */
+  public class Output {
+
+    public void print(String message) {
+      System.out.println(message);
+    }
+
+  }
+
+  /**
+   * The User Input.
+
+   * @author Bryony
+   *
+   */
+  public class Input {
+
+    private BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+    /**
+     * Read the user input.
+
+     * @return The user's input
+     */
+    public String read() {
+      try {
+        return read.readLine();
+      } catch (IOException e) {
+        System.out.println("Something went wrong with System.in");
+        e.printStackTrace();
+        System.exit(0);
+      }
+      return "";
+    }
+  }
+
+}
