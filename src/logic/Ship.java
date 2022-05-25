66f3c27bab38ab9b05af710198a155e309327fe7
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2022-05-25 13:22:55 +1200

    Add methods to Ship
    remove all set methods.
    edit getCargo and getSpeed to account for crew member benefits.
    add heal (increases life if crew includes shipwright), fix (resets life
    when comes into port), and stillAlive (returns true if the life is above
    0).

--- src/logic/Ship.java
+++ src/logic/Ship.java
@@ -15,19 +15,29 @@ public class Ship {
   private int life;
   private int firepower;
   
+  private boolean cargoMaster = false;
+  private boolean navigator = false;
+  private boolean shipwright = false;
+  private int lifeLeft;
+  
   /**
    * Create a ship.
 
    * @param name Ship's name
    * @param price The price of the ship
    * @param cargo The amount of cargo the ship can carry
+   * @param speed The speed of the ship
+   * @param life The life of the ship
+   * @param firepower The number of cannons the ship has
    */
-  protected Ship(String name, int price, int cargo, int speed, int life, int firepower) {
+  protected Ship(String name, int price, int cargo, int speed, 
+      int life, int firepower) {
     this.name = name;
     this.price = price;
     this.cargo = cargo;
     this.speed = speed;
     this.life = life;
+    this.lifeLeft = life;
     this.firepower = firepower;
   }
 
@@ -35,48 +45,79 @@ public class Ship {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public int getPrice() {
     return price;
   }
 
-  public void setPrice(int price) {
-    this.price = price;
-  }
+  /**
+   * The amount of items that can be stored in the cargo.
+   * Is increased by the Cargo Master.
 
+   * @return The cargo size
+   */
   public int getCargo() {
+    if (cargoMaster) {
+      return (int) (cargo * 1.5);
+    }
     return cargo;
   }
 
-  public void setCargo(int cargo) {
-    this.cargo = cargo;
-  }
+  /**
+   * The speed of the ship.
+   * Is increased by the Navigator.
 
+   * @return The speed of the ship.
+   */
   public int getSpeed() {
+    if (navigator) {
+      return (int) (speed + 2);
+    }
     return speed;
-  }
-
-  public void setSpeed(int speed) {
-    this.speed = speed;
   }
 
   public int getLife() {
     return life;
   }
 
-  public void setLife(int life) {
-    this.life = life;
-  }
-
   public int getFirepower() {
     return firepower;
   }
 
-  public void setFirepower(int firepower) {
-    this.firepower = firepower;
+  public int getTradein() {
+    return (int) (price * 0.25);
+  }
+  
+  public void addCargoMaster() {
+    cargoMaster = true;
+  }
+  
+  public void addNavigator() {
+    navigator = true;
+  }
+  
+  public void addShipwright() {
+    shipwright = true;
+  }
+  
+  public void getHit() {
+    lifeLeft -= 20;
+  }
+  
+  /**
+   * The shipwright can fix the ship mid journey.
+   */
+  public void heal() {
+    if (shipwright) {
+      lifeLeft += 10;
+    }
+  }
+  
+  public void fix() {
+    lifeLeft = life;
+  }
+  
+  public boolean stillAlive() {
+    return lifeLeft > 0;
   }
   
 }
