ee1e49c9b9707e2511ce056ea3c5ca3a7ad12c6e
Bryony Gatehouse <gatehobryo@ecs.vuw.ac.nz>
2022-05-27 17:29:49 +1200

    Edit Populate
    No longer static. (prevents overflow of variables from testing).
    Put ships outside for use in testing.
    Put cargomaster outside for testing.

--- src/logic/Populate.java
+++ src/logic/Populate.java
@@ -8,7 +8,12 @@ package logic;
  */
 public class Populate {
 
-  protected static Island[] islands() {
+  /**
+   * An array of islands.
+
+   * @return an array of islands
+   */
+  public Island[] islands() {
     Island[] lands = new Island[5];
     
     lands[0] =  new Island("Dragon Cove", 28, 34, 23, 14);
@@ -20,30 +25,38 @@ public class Populate {
     return lands;
   }
   
-  protected static String homeLand() {
+  protected String homeLand() {
     return "Fort Ridley";
   }
   
-  protected static Ship[] ships() {
-    Ship[] ships = new Ship[7];
-    
-    ships[0] = new Ship("Raft", 0, 20, 6, 30, 0);
-    ships[1] = new Ship("Jimmy Rigger", 3000, 60, 7, 50, 1);
-    ships[2] = new Ship("Carabelle", 9000, 200, 8, 70, 1);
-    ships[3] = new Ship("Sea Sultan", 30000, 600, 9, 90, 2);
-    ships[4] = new Ship("The Koi", 90000, 1800, 12, 100, 3);
-    ships[5] = new Ship("Steaming Fury", 300000, 5000, 12, 140, 4);
-    ships[6] = new Ship("Phoenix Warbird", 1000000, 6000, 13, 180, 5);
-    
+  public Ship raft = new Ship("Raft", 0, 20, 6, 30, 0);
+  public Ship jimmyRigger = new Ship("Jimmy Rigger", 3000, 60, 7, 50, 1);
+  public Ship carabelle = new Ship("Carabelle", 9000, 200, 8, 70, 1);
+  public Ship seaSultan = new Ship("Sea Sultan", 30000, 600, 9, 90, 2);
+  public Ship koi = new Ship("The Koi", 90000, 1800, 12, 100, 3);
+  public Ship steamingFury = new Ship("Steaming Fury", 300000, 5000, 12, 140, 4);
+  public Ship phoenixWarbird = new Ship("Phoenix Warbird", 1000000, 6000, 13, 180, 5);
+  
+  /**
+   * An array of Ships.
+
+   * @return A list of ships
+   */
+  public Ship[] ships() {
+    Ship[] ships = {
+        raft, jimmyRigger, carabelle, seaSultan, koi, steamingFury, phoenixWarbird
+    };
     return ships;
   }
   
-  protected static Crew[] crew() {
+  public Crew cargoMaster = new Crew("Cargo Master", 15000);
+  
+  protected Crew[] crew() {
     Crew[] crew = new Crew[6];
     
     crew[0] = new Crew("Cabin Boy", 0);
     crew[1] = new Crew("Shipwright", 20000);
-    crew[2] = new Crew("Cargo Master", 15000);
+    crew[2] = cargoMaster;
     crew[3] = new Crew("Navigator", 12000);
     crew[4] = new Crew("Cannoneer", 20000);
     crew[5] = new Crew("Cook", 0);
@@ -57,7 +70,7 @@ public class Populate {
    * @param index The index of the item
    * @return The item's name
    */
-  public static String item(int index) {
+  public String item(int index) {
     switch (index) {
       case 0: return "Grain";
       case 1: return "Spices";
